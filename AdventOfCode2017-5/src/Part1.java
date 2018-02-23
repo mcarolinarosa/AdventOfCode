a8996adcfb666e3300f83141865a71afac953082
carolina <mcarolinarosa96@gmail.com>
2018-02-23 13:25:58 +0000

    day 5

---
+++ AdventOfCode2017-5/src/Part1.java
@@ -0,0 +1,35 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.io.*;
+
+public class Part1 {
+
+    public static void main(String [ ] args) throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\carolina\\Desktop\\AdventOfCode\\AdventOfCode2017-5\\src\\file.txt"));
+        String line;
+        int cenas = 0;
+        int count = 0;
+        ArrayList<Integer> jumps = new ArrayList<>();
+
+        while ((line = br.readLine()) != null ) {
+            jumps.add(Integer.parseInt(line));
+        }
+        while (cenas < jumps.size()){
+            int val=jumps.get(cenas);
+            if (val>=3){
+                jumps.set(cenas,val-1);
+
+            }
+            else{
+                jumps.set(cenas,val+1);
+            }
+            cenas=cenas+val;
+
+            count++;
+        }
+
+        System.out.println("count = " + count);
+    }
+}
